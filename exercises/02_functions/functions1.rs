3f4fc3cbbd933739788474126e423a61c562b5ac
nhlmg93 <nhelmig93@gmail.com>
2024-08-07 19:22:06 -0500

    exercises 00-03 and quiz1 finished

--- exercises/02_functions/functions1.rs
+++ exercises/02_functions/functions1.rs
@@ -1,4 +1,7 @@
 // TODO: Add some function with the name `call_me` without arguments or a return value.
+fn call_me() {
+    print!("I'm being called")
+}
 
 fn main() {
     call_me(); // Don't change this line
