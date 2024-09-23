43a64e9b22a4a97a49283c889e6219afb42f7fd3
nhlmg93 <nhelmig93@gmail.com>
2024-09-22 21:07:18 -0500

    hashmaps be hashing

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -31,8 +31,26 @@ fn build_scores_table(results: &str) -> HashMap<&str, Team> {
         // Keep in mind that goals scored by team 1 will be the number of goals
         // conceded by team 2. Similarly, goals scored by team 2 will be the
         // number of goals conceded by team 1.
-    }
+        scores.entry(team_1_name).or_insert(Team {
+            goals_conceded: 0,
+            goals_scored: 0,
+        });
 
+        if let Some(t) = scores.get_mut(team_1_name) {
+            t.goals_scored += team_1_score;
+            t.goals_conceded += team_2_score;
+        }
+
+        scores.entry(team_2_name).or_insert(Team {
+            goals_conceded: 0,
+            goals_scored: 0,
+        });
+
+        if let Some(t) = scores.get_mut(team_2_name) {
+            t.goals_scored += team_2_score;
+            t.goals_conceded += team_1_score;
+        }
+    }
     scores
 }
 
